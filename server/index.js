b89ffba6a89f552a4c67920e0a3f75faf4b4ab29
abhi-0086 <babaabhishek86@gmail.com>
2023-06-16 11:00:47 +0530

    mongodb connection setup done

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+import connectDB from "./mongodb/connect.js";
+
 //configure dotenv
 dotenv.config();
 
@@ -22,9 +24,14 @@ app.get("/", async (req, res) => {
 
 //start server
 const startServer = async () => {
-  app.listen(8080, () =>
-    console.log("Server is running on port http://localhost:8080")
-  );
+  try {
+    connectDB(process.env.MONGO_URL);
+    app.listen(8080, () =>
+      console.log("Server is running on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 //why are we using async await here?
